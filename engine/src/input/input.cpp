5281365a5ff401345762b79ef2a27f8332c24fce
Moldytzu <m.alex123@yahoo.com>
2022-08-23 21:32:22 +0300

    text input

--- engine/src/input/input.cpp
+++ engine/src/input/input.cpp
@@ -1,11 +1,22 @@
 #include <engine/input.h>
 
+Engine::Input::InputManager *_instance;
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    Engine::Core::Logger::LogInfo(std::string(1,(char)key));
+}
+
 void Engine::Input::InputManager::Init()
 {
     Engine::Core::Logger::LogDebug("Initialising the input manager");
+    _instance = this;
+
     Keys = new bool[GLFW_KEY_LAST - GLFW_KEY_SPACE]; // create an array to store the key states
     
     window = Engine::Render::GlobalRenderer->Window; // set our internal glfw window to the renderer's one
+
+    glfwSetKeyCallback(window, keyCallback); // set the callback for key input
 }
 
 Engine::Input::InputManager::InputManager()
